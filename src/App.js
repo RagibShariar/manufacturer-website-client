94090217e5f35f0289fc07a81e7cf1e5661b7b1e
Ragib Shariar <srragib@gmail.com>
2022-05-21 22:40:50 +0600

    tailwind, daisyUI installed

--- src/App.js
+++ src/App.js
@@ -3,21 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div class="drawer drawer-end ">
+  <input id="my-drawer-3" type="checkbox" class="drawer-toggle " /> 
+  <div class="drawer-content flex flex-col ">
+    {/* <!-- Navbar --> */}
+    <div class="w-full navbar  text-slate-400 bg-secondary">
+      
+      <div class="flex-1 px-2 mx-2 ">
+        <img src={logo} alt="" />
+      </div>
+      <div class="flex-none lg:hidden">
+        <label for="my-drawer-3" class="btn btn-square btn-ghost">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+        </label>
+      </div> 
+      <div class="flex-none hidden lg:block">
+        <ul class="menu menu-horizontal">
+          {/* <!-- Navbar menu content here --> */}
+          <li><a>Navbar Item 1</a></li>
+          <li><a>Navbar Item 2</a></li>
+        </ul>
+      </div>
+    </div>
+    {/* <!-- Page content here --> */}
+    Content
+    <h1 className="text-6xl text-primary">
+      Lorem, ipsum dolor.
+    </h1>
+  </div> 
+  <div class="drawer-side">
+    <label for="my-drawer-3" class="drawer-overlay"></label> 
+    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+      {/* <!-- Sidebar content here --> */}
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+      
+    </ul>
+    
+  </div>
+</div>
     </div>
   );
 }
