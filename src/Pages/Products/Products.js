dd66ea4b42d5cfb5d173b58dda63cdde83210a99
Ragib Shariar <srragib@gmail.com>
2022-05-24 03:26:37 +0600

    dot env local added

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useProducts from '../../hooks/useProducts';
+import Product from './Product';
 
 const Products = () => {
+    const [products, setProducts] = useProducts();
+    // const [products, setProducts] = useState([]);
+
+    // // data load
+    // useEffect(() => {
+    //     fetch('switchData.json')
+    //         .then(res => res.json())
+    //         .then(data => setProducts(data));
+    // }, [])
+
     return (
-        <div>
-            <h1 className="text-9xl mt-16">
-                this is products page
-            </h1>
+        <div className=' container'> 
+            <div className='mt-16 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+            {
+                products.length>0 && products.map( product => <Product
+                    key={product._id}
+                    product={product}
+                    >
+                </Product>)
+            }
+            </div>
         </div>
     );
 };
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAnFtWFbwe-MOAlX2YtaQeLdwUGlVH3r2I",
-  authDomain: "manufacturer-website-d6184.firebaseapp.com",
-  projectId: "manufacturer-website-d6184",
-  storageBucket: "manufacturer-website-d6184.appspot.com",
-  messagingSenderId: "723599855058",
-  appId: "1:723599855058:web:9393d4cf47e91bf6c14c0b"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
